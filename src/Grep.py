5f05abcf6779b12410e8ab6c66dbdc2cb77af034
Bryson <bryson.meiling@aggiemail.usu.edu>
2019-01-24 16:07:01 -0700

    grep is working! I havne't tried all edge cases though

--- src/Grep.py
+++ src/Grep.py
@@ -4,16 +4,44 @@ from Usage import usage
 def grep(args):
 
     if args[0] == '-v':
-        exclude(args[1:])
+        exclude(args[2:], args[1])
     else:
-        include(args)
+        include(args[1:], args[0])
 
 
-def include(args):
+def include(files: list, iden: str):
     print("")
-    pass
+    for file in files:
+        try:
+            with open(file) as data:
+                lines = data.readlines()
+
+            # remove \n left over from .readlines()
+            for i in range(len(lines)):
+                lines[i] = lines[i][:-1]
+
+            for line in lines:
+                if line.find(iden) != -1:
+                    print(line)
+
+        except FileNotFoundError:
+            usage("\n    Given file not found.", "grep")
 
 
-def exclude(args):
+def exclude(files: list, iden:str):
     print("")
-    pass
+    for file in files:
+        try:
+            with open(file) as data:
+                lines = data.readlines()
+
+            # remove \n left over from .readlines()
+            for i in range(len(lines)):
+                lines[i] = lines[i][:-1]
+
+            for line in lines:
+                if line.find(iden) == -1:
+                    print(line)
+
+        except FileNotFoundError:
+            usage("\n    Given file not found.", "grep")
