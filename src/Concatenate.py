8ff503a5e967e386deecec0b56ddd2108d240210
Bryson <bryson.meiling@aggiemail.usu.edu>
2019-01-24 10:49:14 -0700

    cat and tac work!

--- src/Concatenate.py
+++ src/Concatenate.py
@@ -1,17 +1,24 @@
 def cat(args):
+    print("")
     try:
         for file in args:
-            with open(file) as myfile:
-                print(myfile.readlines())
+            with open(file) as my_file:
+                data = my_file.readlines()
+                for line in data:
+                        print(line[:-1 or None])
     except FileNotFoundError:
         print("Given file not found.")
 
 
 def tac(args):
+    print("")
     try:
         for file in args:
-            with open(file) as myfile:
-                print(myfile.readlines(-1))
+            with open(file) as my_file:
+                data = my_file.readlines()
+                for i in reversed(range(len(data))):
+                    line = data[i]
+                    print(line[:-1 or None])
     except FileNotFoundError:
         print("Given file not found.")
 
