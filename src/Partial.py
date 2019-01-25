758128399273084d20c51c0ca75ca7e08aeee438
Bryson <bryson.meiling@aggiemail.usu.edu>
2019-01-25 01:01:13 -0700

    partial works

--- src/Partial.py
+++ src/Partial.py
@@ -1,18 +1,104 @@
 def head(args):
     """output the first part of files"""
 
+
+# a specific amount of head
     if args[0] == '-n':
-        # other than the first 10
+        size = int(args[1])
+        args = args[2:]
+        for file in args:
+            try:
+                with open(file) as my_file:
+                    data = my_file.readlines()
+
+                # remove \n left over from .readlines()
+                for i in range(len(data)):
+                    data[i] = data[i][:-1]
+
+                for i in range(size):
+                    print(data[i])
+
+            except FileNotFoundError:
+                print("The file given was not located.")
+            except ValueError:
+                print("Argument after -n must be an integer")
+            except IndexError:
+                pass
+
+# default prints 10 lines
     else:
-        # first 10
+        for file in args:
+            try:
+                with open(file) as my_file:
+                    data = my_file.readlines()
+
+                # remove \n left over from .readlines()
+                for i in range(len(data)):
+                    data[i] = data[i][:-1]
+
+                for i in range(10):
+                    print(data[i])
+
+            except FileNotFoundError:
+                print("The file given was not located.")
+            except ValueError:
+                print("Argument after -n must be an integer")
+            except IndexError:
+                pass
 
 
 def tail(args):
     """output the last part of files"""
 
     if args[0] == '-n':
-        # other than the last 10
-    else:
-        # last 10
+        size = int(args[1])
+        args = args[2:]
+        for file in args:
+            try:
+                with open(file) as my_file:
+                    data = my_file.readlines()
 
+                # remove \n left over from file.readlines()
+                for i in range(len(data)):
+                    data[i] = data[i][:-1]
+
+                j = 0
+                for i in reversed(range(len(data))):
+                    if j >= size:
+                        pass
+                    else:
+                        print(data[i])
+                        j += 1
+
+            except FileNotFoundError:
+                print("The file given was not located.")
+            except ValueError:
+                print("Argument after -n must be an integer")
+            except IndexError:
+                pass
+
+    else:
+        for file in args:
+            try:
+                with open(file) as my_file:
+                    data = my_file.readlines()
+
+                # remove \n left over from file.readlines()
+                for i in range(len(data)):
+                    data[i] = data[i][:-1]
+
+                j = 0
+                for i in reversed(range(len(data))):
+                    if j >= 10:
+                        pass
+                    else:
+                        print(data[i])
+                        j += 1
+
+            except FileNotFoundError:
+                print("The file given was not located.")
+            except ValueError:
+                print("Argument after -n must be an integer")
+            except IndexError:
+                print("index error")
 
