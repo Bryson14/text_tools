c50ddf4ca9d0c4aa8354c6c916531539012b7e3a
Bryson <bryson.meiling@aggiemail.usu.edu>
2019-01-26 11:09:10 -0700

    cut and paste is a mess, sorting almost words except for make_word_list

--- src/CutPaste.py
+++ src/CutPaste.py
@@ -1,8 +1,43 @@
+from Usage import usage
+
+
 def cut(args):
     """remove sections from each line of files"""
-    pass
+
+    if args[0] == "-f":
+
+        # takes in one or multiple column args
+        iden = str(args[1])
+        if "," in iden:
+            iden.split(",")
+        else:
+            iden = int(iden)
+
+    else:
+        print_cut(args[0])
+
+def print_cut(file):
+    try:
+        with open(file) as data:
+            lines = data.readlines()
+
+        # remove \n left over from .readlines()
+        for i in range(len(lines)):
+            lines[i] = lines[i][:-1]
+
+        # splits lines by commas into lists
+        for i in range(len(lines)):
+            lines[i] = lines[i].split(",")
+
+        for line in lines:
+            print(line[0])
+
+    except FileNotFoundError:
+        usage("File not found", "cut")
 
 
 def paste(args):
     """merge lines of files"""
-    pass
+
+
+
--- src/Sorting.py
+++ src/Sorting.py
@@ -1,3 +1,6 @@
+from Usage import usage
+
+
 def sort(args):
     """sort lines of text files"""
     pass
@@ -5,4 +8,110 @@ def sort(args):
 
 def uniq(args):
     """report or omit repeated lines"""
-    pass
+
+# prints only the unique lines
+    if args[0] == "-u":
+        files = args[1:]
+        for file in files:
+            omit_copies(file)
+
+# prints only the duplicates lines
+    elif args[0] == "-D":
+        files = args[1:]
+        for file in files:
+            omit_uniq(file)
+
+# returns the # of occurrences of each line
+    elif args[0] == "-c":
+        files = args[1:]
+        for file in files:
+            count(file)
+
+# prints every line only once despite duplicates
+    else:
+        files = args[1:]
+        for file in files:
+            once = only_once(file)
+            for o in once:
+                print(o)
+
+
+def omit_copies(file):
+    lst = word_list(file)
+    for i in range(len(lst)):
+        j = i + 1
+
+        # end of list
+        if j > len(lst):
+            if lst[i] != list[j -2]:
+                print(lst[i])
+
+        # does not print line i
+        elif lst[i] == lst[j]:
+            pass
+
+        else:
+            print(lst[i])
+
+
+def omit_uniq(file):
+    lst = word_list(file)
+    for i in range(len(lst)):
+        j = i + 1
+
+        # end of list
+        if j >= len(lst):
+            if lst[i] != lst[j - 2]:
+                print(lst[i])
+
+        # print line i with dup
+        elif lst[i] == lst[j]:
+            print(lst[i])
+
+        # backwards check
+        elif i > 0:
+            if lst[i] == lst[j -2]:
+                print(lst[i])
+        else:
+            pass
+
+
+def count(file):
+    word_lst = word_list(file)
+    uniqs = only_once(file)
+    occur = []
+
+    # makes a separate list with the number of occurrences
+    for unq in uniqs:
+        occur.append(word_lst.count(unq))
+
+    for i in range(len(occur)):
+        print(occur[i], " : ", word_lst[i])
+
+
+def only_once(file) -> list:
+    lst = word_list(file)
+    once_lst = []
+    for i in range(len(lst)):
+        j = i -1
+
+        # beginning of list
+        if j < 0:
+            once_lst.append(lst[i])
+
+        # backwards check
+        elif i > 0:
+            if lst[i] != lst[j]:
+                once_lst.append(lst[i])
+        else:
+            pass
+
+    return once_lst
+
+def word_list(file) -> list:
+    try:
+        with open(file) as data:
+            data = data.read()
+        data.split(" ")
+    except FileNotFoundError:
+        usage("File not found", "sort")
